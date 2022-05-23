129982461e71cc55b13663f5bcfd4611ede6be6b
Allison Bohannon <akboh.github@gmail.com>
2022-05-22 20:18:23 -0700

    commit tree 9134e8c4ad7ca2a5e386866dd9a4316121ad8dd6

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let check = true; 
+  for (let i = 0; i < word.length/2; i++) {
+    console.log(i)
+    console.log(`${word[i]}, ${word[word.length-i-1]}`)
+    if(word[i] !== word[word.length - i-1]) {
+      check = false; 
+      break
+    } 
+  }
+  return check
 }
 
 /* 
   Add your pseudocode here
+
+  const check = true; 
+  for (i, i < array.length/2, i++) 
+  { if (array[i] ===  array[array.length - i]) 
+  {
+
+  }}
 */
 
 /*
   Add written explanation of your solution here
+  Check that the letter in the 1st position matches the letter in the last position
+    Create a variable check that equals True
+    Use a for loop to iterate through 1/2 the length of the array
+    At each loop, evalute whether the ith letter = 1-ith letter
+    If they match, i++, if they don't match, set check = False
+    After loop has completed, return check variable
+  
 */
 
 // You can run `node index.js` to view these console logs
